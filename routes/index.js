515cae048bfe2a17608a2c5f9d8b24ac75a5e3c0
quevedoclara <clara.quevedo.galan@alumnos.upm.es>
2016-04-26 23:10:02 +0200

    GET/ Author en rama créditos

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET /author */
+router.get('/author', function(req, res, next) {
+	res.render('author', {author: {name: 'Clara Quevedo Galán', photo: 'images/me.png' }});
+
+});
+
 module.exports = router;
