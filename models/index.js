ff4651c24f82646300462dece2cbb3e1d79553ec
quevedoclara <clara.quevedo.galan@alumnos.upm.es>
2016-05-26 17:41:04 +0200

    Tema 10: Múltiples preguntas

--- models/index.js
+++ models/index.js
@@ -1,45 +1,37 @@
-var path = require('path');
+	var path = require('path');
 
- // Cargar Modelo ORM
- var Sequelize = require('sequelize');
+	// Cargar Modelo ORM
+	var Sequelize = require('sequelize');
+	var url, storage;
+	if(!process.env.DATABASE_URL){
+	url = "sqlite:///";
+	storage= "quiz.sqlite";
+	}else{
+	url= process.env.DATABASE_URL;
+	storage =process.env.DATABASE_STORAGE || "";
+	}
 
- var url, storage;
+	var sequelize = new Sequelize( url, { storage: storage, omitNull:true} );
+	var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
- if(!process.env.DATABASE_URL){
- url = "sqlite:///";
- storage= "quiz.sqlite";
+	// Importar la definicion de la tabla Quiz de quiz.js
+	sequelize.sync()
+	.then(function() {
+	return Quiz.count()
+	.then(function (c) {
+	if (c === 0) { // la tabla se inicializa solo si está vacía
+	 return Quiz.bulkCreate([ {question: 'Capital de Italia', answer: 'Roma'},
+	 {question: 'Capital de Portugal', answer: 'Lisboa'}
+	 ])
+	 .then(function() {
+	 console.log('Base de datos inicializada con datos');
+	 });
+	}
+	});
+	 })
+	 .catch(function(error) {
+	 console.log("Error Sincronizando las tablas de la BBDD:", error);
+	 process.exit(1);
+	});
 
- }else{
- url= process.env.DATABASE_URL;
- storage =process.env.DATABASE_STORAGE || "";
-
- }
-
- // Usar BBDD SQLite:
- var sequelize = new Sequelize( url, { storage: storage, omitNull:true} );
-
- // Importar la definicion de la tabla Quiz de quiz.js
- var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-
- sequelize.sync()
- .then(function() {
- // Ya se han creado las tablas necesarias.
- return Quiz.count()
- .then(function (c) {
- if (c === 0) { // la tabla se inicializa solo si está vacía
- return Quiz.create({ question: 'Capital de Italia',
- answer: 'Roma'
- })
- .then(function() {
- console.log('Base de datos inicializada con datos');
- });
- }
- });
- })
- .catch(function(error) {
- console.log("Error Sincronizando las tablas de la BBDD:", error);
- process.exit(1);
- });
-
-
- exports.Quiz = Quiz; // exportar definición de tabla Quiz
+	exports.Quiz = Quiz; // exportar definición de tabla Quiz 
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var quizController = require('../controllers/quiz_controller');
-
+var quizController = require ('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+res.render('index');
 });
-
 router.get('/author', function(req, res, next) {
-	res.render('author', { title: 'Author' });
+res.render('author', { title: 'Author' });
 });
 
-router.get('/question', quizController.question);
-router.get('/check', quizController.check);
-
+router.get('/quizzes', quizController.index);
+router.get('/quizzes/:quizId(\\d+)', quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 module.exports = router;
