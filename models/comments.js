ae6f7fd459574954c1912ffe87b5cda01559d630
quevedoclara <clara.quevedo.galan@alumnos.upm.es>
2016-05-26 20:56:14 +0200

    Tema 20: Gestión de Usuarios

---
+++ models/comments.js
@@ -0,0 +1,7 @@
+module.exports = function(sequelize, DataTypes) {
+return sequelize.define('Comment',
+{ text: { type: DataTypes.STRING,
+validate: { notEmpty: {msg: "Falta Comentario"}}
+}
+});
+};
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require ('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 res.render('index');
@@ -10,6 +11,17 @@ router.get('/author', function(req, res, next) {
 res.render('author', { title: 'Author' });
 });
 router.param('quizId', quizController.load);
+router.param('userId', userController.load); // autoload :userId
+
+
+// Definición de rutas de cuenta
+router.get('/users', userController.index); // listado usuarios
+router.get('/users/:userId(\\d+)', userController.show); // ver un usuario
+router.get('/users/new', userController.new); // formulario sign un
+router.post('/users', userController.create); // registrar usuario
+router.get('/users/:userId(\\d+)/edit', userController.edit); // editar información de cuenta
+router.put('/users/:userId(\\d+)', userController.update); // actualizar información de cuenta
+router.delete('/users/:userId(\\d+)', userController.destroy); // borrar cuenta
 router.get('/quizzes:format?', quizController.index);
 router.get('/quizzes/:quizId(\\d+):format?', quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
