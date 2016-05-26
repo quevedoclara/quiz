0dc54160d651cb5eba9b24142da2becbf6eed473
quevedoclara <clara.quevedo.galan@alumnos.upm.es>
2016-05-26 20:13:12 +0200

    Tema 17: Migraciones y seeders

--- models/index.js
+++ models/index.js
@@ -1,28 +1,28 @@
 var path = require('path');
-
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 var url, storage;
 if(!process.env.DATABASE_URL){
-	url = "sqlite:///";
-	storage= "quiz.sqlite";
+url = "sqlite:///";
+storage= "quiz.sqlite";
 }else{
-	url= process.env.DATABASE_URL;
-	storage =process.env.DATABASE_STORAGE || "";
+url = process.env.DATABASE_URL;
+storage =process.env.DATABASE_STORAGE || "";
 }
 
 var sequelize = new Sequelize( url, { storage: storage, omitNull:true} );
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-
 // Importar la definicion de la tabla Quiz de quiz.js
-sequelize.sync()
+
+/* sequelize.sync()
 .then(function() {
 return Quiz.count()
 .then(function (c) {
 if (c === 0) { // la tabla se inicializa solo si está vacía
-	return Quiz.bulkCreate([{question: 'Capital de Italia', answer: 'Roma'},
-							{question: 'Capital de Portugal', answer: 'Lisboa'},
-							{question: 'Capital de Argentina', answer: 'Buenos Aires'}])
+return Quiz.bulkCreate([ {question: 'Capital de Italia', answer: 'Roma'},
+{question: 'Capital de Portugal', answer: 'Lisboa'},
+{question: 'Capital de Argentina', answer: 'Buenos Aires'}
+])
 .then(function() {
 console.log('Base de datos inicializada con datos');
 });
@@ -32,5 +32,6 @@ console.log('Base de datos inicializada con datos');
 .catch(function(error) {
 console.log("Error Sincronizando las tablas de la BBDD:", error);
 process.exit(1);
-});
+}); */
+
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
