204a4afadbf5a45845b362d331267cc8ad4eb56a
quevedoclara <clara.quevedo.galan@alumnos.upm.es>
2016-05-30 23:49:24 +0200

    Tema 23: Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,38 +1,35 @@
 var userController = require('./user_controller');
-
-
 // GET /session -- Formulario de login
 exports.new = function(req, res, next) {
- res.render('session/new');
+
+ res.render('session/new', { redir: req.query.redir || '/' });
 };
-
-
+exports.loginRequired = function (req, res, next) {
+ if (req.session.user) {
+ next();
+ } else {
+ res.redirect('/session?redir=' + (req.param('redir') || req.url));
+ }
+};
 // POST /session -- Crear la sesion si usuario se autentica
 exports.create = function(req, res, next) {
-
- var login = req.body.login;
- var password = req.body.password;
-
- userController.autenticar(login, password)
- .then(function(user) {
-
- // Crear req.session.user y guardar campos id y username
- // La sesión se define por la existencia de: req.session.user
- req.session.user = {id:user.id, username:user.username};
-
- res.redirect("/"); // redirección a la raiz
- })
- .catch(function(error) {
- req.flash('error', 'Se ha producido un error: ' + error);
- res.redirect("/session");
- });
+var login = req.body.login;
+var password = req.body.password;
+var redir = req.body.redir || '/'
+userController.autenticar(login, password)
+.then(function(user) {
+// Crear req.session.user y guardar campos id y username
+// La sesión se define por la existencia de: req.session.user
+req.session.user = {id:user.id, username:user.username};
+ res.redirect(redir); // redirección a la raiz
+})
+.catch(function(error) {
+req.flash('error', 'Se ha producido un error: ' + error);
+ res.redirect("/session?redir="+redir);
+});
 };
-
-
 // DELETE /session -- Destruir sesion
 exports.destroy = function(req, res, next) {
-
- delete req.session.user;
-
- res.redirect("/session"); // redirect a login
+delete req.session.user;
+res.redirect("/session"); // redirect a login
 };
